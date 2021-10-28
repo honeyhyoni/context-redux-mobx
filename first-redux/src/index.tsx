67ca19829c53904b62faeff989eafcac4a91b2f6
suhyeonP <ahah12k@gmail.com>
2021-10-28 10:18:27 +0900

    :art: persist

--- first-redux/src/index.tsx
+++ first-redux/src/index.tsx
@@ -1,17 +1,38 @@
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import store from "./store";
+// import {Provider} from "react-redux";
+//
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <App />
+//     </Provider>,
+//   document.getElementById('root')
+// );
+//
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+// 기존의 index.tsx
+
+import React from "react";
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import store from "./store";
-import {Provider} from "react-redux";
+import { Provider } from 'react-redux';
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import App from './App';
 
-ReactDOM.render(
+const reduxPersist = persistStore(store);
+
+const Root = () => (
     <Provider store={store}>
-        <App />
-    </Provider>,
-  document.getElementById('root')
-);
+        <PersistGate persistor={reduxPersist} loading={null}>
+            <App/>
+        </PersistGate>
+    </Provider>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Root />, document.getElementById('root'));
--- first-redux/src/reducer/index.ts
+++ first-redux/src/reducer/index.ts
@@ -1,16 +1,34 @@
 import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
+// localStorage 에 저장하고싶을때
+
+// import storageSession from 'redux-persist/lib/storage/session';
+// sessionStorage 에 저장하고 싶을때
+
+import { persistReducer } from 'redux-persist';
 import counter from "./counter";
+
+
+const persistConfig = {
+    key: "root",
+    // localStorage에 저장합니다.
+    storage,
+    // reducer 중에 whitelist에 포함된 reducer만 localstorage에 저장합니다.
+    whitelist: ["counter"],
+    // blacklist -> 그것만 제외합니다
+    blacklist: []
+};
 
 // (이전상태, 액션) => 다음상태
 const rootReducer = combineReducers({
-    index: (state = {}, action) => {
-        switch (action.type) {
-            default:
-                return state;
-        }
-    },
+    // index: (state = {}, action) => {
+    //     switch (action.type) {
+    //         default:
+    //             return state;
+    //     }
+    // },
     counter
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
 export type RootState = ReturnType<typeof rootReducer>;
